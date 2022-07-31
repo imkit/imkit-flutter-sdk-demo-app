c1d2d564e5953a4e694ada1a3ef37c575f61feb4
Tzuwei Chen <chen.tzuwei@gmail.com>
2022-07-31 17:13:24 +0800

    feat: setPackageName to SDK for assets path

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ Future<void> main() async {
     ..setTranslationApiKey("AIzaSyANXUoLgkHrCax6dct18QledIBA550lcuk")
     ..setUid("ho2")
     ..setToken(
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImhvMiIsImV4cCI6MTY4MDIzNjI2NiwiaWF0IjoxNjQ4NzAwMjY2fQ.RNnKq73uj43rzlt69sVkfaA46sPy4YA_9zJJwQRnErU"));
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImhvMiIsImV4cCI6MTY4MDIzNjI2NiwiaWF0IjoxNjQ4NzAwMjY2fQ.RNnKq73uj43rzlt69sVkfaA46sPy4YA_9zJJwQRnErU")
+    ..setPackageName("imkit"));
 
   // IMKit.instance.connect();
 
