31a74027e0e8f1ef72a8392fad2ff518cad80e57
Tzuwei Chen <chen.tzuwei@gmail.com>
2022-07-31 20:35:11 +0800

    feat: using default sticker in SDK

--- lib/main.dart
+++ lib/main.dart
@@ -4,37 +4,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_imkit_demo/app.dart';
 import 'package:imkit/imkit_sdk.dart';
 
-import 'gen/assets.gen.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // TODO may find a way auto input sticker folder's file
-  List<String> getStickers() {
-    List<String> sticker = [
-      Assets.stickers.sticker1.path,
-      Assets.stickers.sticker2.path,
-      Assets.stickers.sticker3.path,
-      Assets.stickers.sticker4.path,
-      Assets.stickers.sticker5.path,
-      Assets.stickers.sticker6.path,
-      Assets.stickers.sticker7.path,
-      Assets.stickers.sticker8.path,
-      Assets.stickers.sticker9.path,
-      Assets.stickers.sticker10.path
-    ];
-
-    return sticker;
-  }
-
   await IMKit.instance.init(IMStateBuilder()
-    ..setClientKey("fangho_imkit_0412_2018_001_clientkey")
-    ..setChatServerURL("https://chat.fangho.com")
-    ..setTranslationApiKey("AIzaSyANXUoLgkHrCax6dct18QledIBA550lcuk")
-    ..setUid("ho2")
-    ..setToken(
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImhvMiIsImV4cCI6MTY4MDIzNjI2NiwiaWF0IjoxNjQ4NzAwMjY2fQ.RNnKq73uj43rzlt69sVkfaA46sPy4YA_9zJJwQRnErU")
-    ..setStickers(getStickers()));
+        ..setClientKey("fangho_imkit_0412_2018_001_clientkey")
+        ..setChatServerURL("https://chat.fangho.com")
+        ..setTranslationApiKey("AIzaSyANXUoLgkHrCax6dct18QledIBA550lcuk")
+        ..setUid("ho2")
+        ..setToken(
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImhvMiIsImV4cCI6MTY4MDIzNjI2NiwiaWF0IjoxNjQ4NzAwMjY2fQ.RNnKq73uj43rzlt69sVkfaA46sPy4YA_9zJJwQRnErU")
+        ..setSDKPackageName("imkit")
+      // set customize sticker if need
+      // ..setStickers([
+      //   Assets.stickers.sticker1.path,
+      //   Assets.stickers.sticker2.path,
+      //   Assets.stickers.sticker3.path,
+      //   Assets.stickers.sticker4.path,
+      //   Assets.stickers.sticker5.path,
+      //   Assets.stickers.sticker6.path,
+      //   Assets.stickers.sticker7.path,
+      //   Assets.stickers.sticker8.path,
+      //   Assets.stickers.sticker9.path,
+      //   Assets.stickers.sticker10.path
+      // ])
+      );
 
   // IMKit.instance.connect();
 
