7b62b632a41ed3cee66b19e4cef600bd6fc2e6e8
11 <iMill1611@gmail.com>
2022-12-03 16:18:32 +0800

    update IMKit init

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await IMKit.init(IMStateBuilder()
-        ..setClientKey("fangho_imkit_0412_2018_001_clientkey")
-        ..setChatServerURL("https://chat.fangho.com")
-        ..setTranslationApiKey("AIzaSyANXUoLgkHrCax6dct18QledIBA550lcuk")
-        ..setSDKPackageName("imkit")
-      // set customize sticker if need
-      // ..setStickers([
-      //   Assets.stickers.sticker1.path,
-      //   Assets.stickers.sticker2.path,
-      //   Assets.stickers.sticker3.path,
-      //   Assets.stickers.sticker4.path,
-      //   Assets.stickers.sticker5.path,
-      //   Assets.stickers.sticker6.path,
-      //   Assets.stickers.sticker7.path,
-      //   Assets.stickers.sticker8.path,
-      //   Assets.stickers.sticker9.path,
-      //   Assets.stickers.sticker10.path
-      // ])
-      );
+    ..setClientKey("fangho_imkit_0412_2018_001_clientkey")
+    ..setChatServerURL("https://chat.fangho.com")
+    ..setSDKPackageName("imkit"));
 
   await IMKit.login(uid: "ho2");
   // IMKit.instance.connect();
